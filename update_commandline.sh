7207a542f7c548a5ac309bb0f83f5bf95f717447
Chadwick Boggs <chad_boggs@cable.comcast.com>
2015-10-01 15:50:38 -0600

    Updates.

--- update_commandline.sh
+++ update_commandline.sh
@@ -4,8 +4,10 @@ sudo nice port selfupdate
 sudo nice port upgrade outdated
 #sudo nice port upgrade installed
 
-#sudo nice fink -qy selfupdate
+sudo nice fink -qy selfupdate
+#sudo nice fink -qy selfupdate-rsync
 #sudo nice fink -qy update-all
+sudo nice fink -qy index -f
 
 sudo nice gem2.1 update
 
