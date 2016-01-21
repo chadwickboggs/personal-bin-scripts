41eb6ece682cd5095d70a13673a9eea38d3f7c0c
Chadwick Boggs <Chadwick.Boggs@charter.com>
2016-01-21 13:27:00 -0700

    Updates.

--- sync_folders.sh
+++ sync_folders.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
 script_home=$(dirname $0)
-now=$(${script_home}/now)
 
+now=$(${script_home}/now)
 echo "[${now}] Syncing folders..."
 
-${script_home}/rsync_cp -v ~/Dropbox/charter ~/google_drive
-${script_home}/rsync_cp -v ~/google_drive/charter ~/Dropbox
+echo "	Synching each into Dropbox..."
 
-${script_home}/rsync_cp -v ~/Dropbox/charter ~/ownCloud
-${script_home}/rsync_cp -v ~/Dropbox/charter ~/amazon_cloud_drive
+${script_home}/rsync_cp -v ~/own_cloud/charter 				~/Dropbox
+${script_home}/rsync_cp -v ~/google_drive/charter 			~/Dropbox
+${script_home}/rsync_cp -v ~/amazon_cloud_drive/charter 	~/Dropbox
+${script_home}/rsync_cp -v ~/icloud/charter 				~/Dropbox
 
+echo "	Done synching each into Dropbox."
+echo "	Synching Dropbox out to each..."
+
+${script_home}/rsync_cp -v ~/Dropbox/charter 	~/own_cloud
+${script_home}/rsync_cp -v ~/Dropbox/charter 	~/google_drive
+${script_home}/rsync_cp -v ~/Dropbox/charter 	~/amazon_cloud_drive
+${script_home}/rsync_cp -v ~/Dropbox/charter 	~/icloud
+
+echo "	Done synching Dropbox out to each."
+
+now=$(${script_home}/now)
 echo "[${now}] Done syncing folders."
