f41b5cdea48a46b3623cc3a863771515568f5427
Chadwick Boggs <Chadwick.Boggs@charter.com>
2017-06-08 09:01:50 -0600

    Updates.

--- touch_create_todays_tomcat_logs.fish
+++ touch_create_todays_tomcat_logs.fish
@@ -2,6 +2,7 @@
 
 set yesterday (date '+%Y-%m-%d' -d 'yesterday')
 set today (date '+%Y-%m-%d')
+set include_localhost_access_log 0
 
 set USAGE "touch_create_tomcat_logs.fish <options>
 
@@ -12,9 +13,10 @@ set USAGE "touch_create_tomcat_logs.fish <options>
 \t\t		[-h|--help]\t\t\t	This message gets printed only.
 \t\t		[-f|--from] <the from string>\t	The date string to replace in the filenames.  Default: \"$yesterday\"
 \t\t		[-t|--to] <the to string>\t	The date string to replace with in the filenames.  Default: \"$today\"
+\t\t		[-i|--include]\t\t	Included the \"localhost_access_log.<from date>.txt\" file also.
 "
 
-set args (getopt -o "hf:t:" -l "help,from,to" -- "$argv")
+set args (getopt -o "hf:t:i" -l "help,from,to,include" -- "$argv")
 set args (fish -c "for el in $args; echo \$el; end")
 set i 1
 while true
@@ -35,6 +37,8 @@ while true
 		case "--to"
 			set i (math "$i + 1")
 			set today "$args[$i]"
+		case "-i"
+			set include_localhost_access_log 1
 		case "--"
 			break
   end
@@ -45,6 +49,10 @@ set yesterday (echo -n $yesterday | sed 's/ //')
 set today (echo -n $today | sed 's/ //')
 
 ls *.$yesterday.log | parallel "touch (echo -n {} | sed s/$yesterday/$today/); rm -v {}"
+
+if test $include_localhost_access_log -eq 1
+	rm -f localhost_access_log.$yesterday.txt
+end
 
 echo Touched: (ls *.$today.log)
 
