c29963719817d7cd80776745f054a0c0c55a61d2
Chadwick Boggs <chadwickboggs@gmail.com>
2018-03-13 19:01:28 -0600

    Updates.

--- update_commandline.sh
+++ update_commandline.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-sudo nice port selfupdate
-sudo nice port upgrade outdated
-#sudo nice port upgrade installed
+pls nice port selfupdate
+pls nice port upgrade outdated
+#pls nice port upgrade installed
 
-#sudo nice fink -qy selfupdate
-#sudo nice fink -qy selfupdate-rsync
-#sudo nice fink -qy update-all
-#sudo nice fink -qy index -f
+#pls nice fink -qy selfupdate
+#pls nice fink -qy selfupdate-rsync
+#pls nice fink -qy update-all
+#pls nice fink -qy index -f
 
-sudo nice npm upgrade
-sudo nice gem update
+pls nice npm upgrade
+pls nice gem update
 
 exit $?
