e2972323cdcf5779a79f172367740a22cb057253
Chadwick Boggs <chadwickboggs@gmail.com>
2018-04-25 13:46:45 -0600

    Updates.

--- set_docker_env.fish
+++ set_docker_env.fish
@@ -2,6 +2,10 @@
 
 set -x DOCKER_TLS_VERIFY "1"
 set -x DOCKER_HOST "tcp://192.168.99.100:2376"
-set -x DOCKER_CERT_PATH "/Users/cboggs1/.docker/machine/machines/default"
-set -x DOCKER_MACHINE_NAME "default"
+
+#set -x DOCKER_CERT_PATH "/Users/cboggs1/.docker/machine/machines/default"
+#set -x DOCKER_MACHINE_NAME "default"
+
+set -x DOCKER_CERT_PATH "/Users/cboggs1/.docker/machine/machines/pht"
+set -x DOCKER_MACHINE_NAME "pht"
 
